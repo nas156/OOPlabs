7f795c9955a2f929fd4cbe1e7f3b5a340c3623f4
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-03-01 11:53:11 +0200

    init form teacher

--- src/Labor2.java
+++ src/Labor2.java
@@ -9,15 +9,16 @@ public class Labor2 {
                     {1, 2, 3, 4, 5},
                     {1, 2, 3, 4, 5}};
             double[][] z= new double[c.length][c[0].length];
+            Scanner scanner = new Scanner(System.in);
             while (!flag){
                 System.out.println("Enter a: ");
-                Scanner scanner = new Scanner(System.in);
                 if (scanner.hasNextInt()) {
                     a = scanner.nextInt();
                     flag = true;
                 }
                 else {
                     System.out.println("You have to enter an int");
+                    scanner.next();
                 }
             }
             System.out.println("\nMatrix A:");
@@ -37,14 +38,15 @@ public class Labor2 {
                 }
                 System.out.println();
             }
-            double n = z[0][0], S = 0;
+            double columnMax, S = 0;
             for (int i = 0; i < c[0].length; i++) {
-                for (int j = 0; j < c.length; j++) {
-                    if (z[j][i] > n) {
-                        n = z[j][i];
+                columnMax = z[0][i];
+                for (int j = 1; j < c.length; j++) {
+                    if (z[j][i] > columnMax) {
+                        columnMax = z[j][i];
                     }
                 }
-                S += n;
+                S += columnMax;
             }
             System.out.println("\nResult: " + S);
         }
