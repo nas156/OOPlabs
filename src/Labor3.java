f91e7de0238ca6b662e3f39d54de0c9433fa94f4
gevel <gevelencko8@gmail.com>
2019-03-14 23:25:07 +0200

    initial commit

--- src/Labor3.java
+++ src/Labor3.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Labor3 {
     public static void main(String[] args) {
         final int a=5;
-        char [] bukva={'b', 'c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z'};
+        char [] bukva={'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z'};
         StringBuilder z = new StringBuilder("Hello, kash, fkslds dkslajkfkla traaa. fsss. fakfaskklfa. Marka natasha. aovba");
         String[] b= (z.toString()).split(" ");
         z.delete(0,z.length());
@@ -12,19 +11,19 @@ public class Labor3 {
             if (b[i].length()!=a&& b[i].length()!=(a+1))
                 z.append(b[i]+" ");
             else
-                if (Arrays.binarySearch(bukva,(((b[i].toLowerCase()).toCharArray())[0]))>0)
-                    {
-                        if ((b[i].length()==(a+1)&&((b[i].toCharArray())[b[i].length()-1]==','|| ((b[i].toCharArray())[b[i].length()-1]=='.'))))
-                            z.append((b[i].toCharArray())[b[i].length()-1]+" ");
-                        else
-                            if (((b[i].length()==a) &&((b[i].toCharArray())[b[i].length()-1]==',' || ((b[i].toCharArray())[b[i].length()-1]=='.'))))
-                                z.append(b[i]+" ");
-                            else z.append(b[i]+" ");
-
-                    }
-                else z.append(b[i]);
+            if (Arrays.binarySearch(bukva,(((b[i].toLowerCase()).toCharArray())[0]))>0)
+            {
+                if (((b[i].length()==a) &&((b[i].toCharArray())[b[i].length()-1]==',' || ((b[i].toCharArray())[b[i].length()-1]=='.'))))
+                    z.append(b[i]+" ");
+                else
+                    if ((b[i].length()==(a+1)&&((b[i].toCharArray())[b[i].length()-1]==','|| ((b[i].toCharArray())[b[i].length()-1]=='.'))))
+                        z.append((b[i].toCharArray())[b[i].length()-1]+" ");
+                    else
+                        if (b[i].length()==a+1)
+                            z.append(b[i]+" ");
+            }
+            else z.append(b[i]);
         }
-
         System.out.println(z.toString());
     }
 }
