2f84eeff637e2f08d46a6838a6aa842e30be9515
gevel <gevelencko8@gmail.com>
2019-03-14 21:10:43 +0200

    initial commit

--- src/Labor3.java
+++ src/Labor3.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -6,7 +5,7 @@ public class Labor3 {
     public static void main(String[] args) {
         final int a=5;
         char [] bukva={'b',  'c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z'};
-        StringBuilder z = new StringBuilder("Hello, kash, fkslds dkslajkfkla fs fakfaskklfa. Marka, natasha. aovba");
+        StringBuilder z = new StringBuilder("Hello, kash, fkslds dkslajkfkla fsss. fakfaskklfa. Marka natasha. aovba");
         String[] b= (z.toString()).split(" ");
         z.delete(0,z.length());
         ArrayList<String> l= new ArrayList<>();
@@ -17,17 +16,10 @@ public class Labor3 {
         for (int i = 0; i <b.length ; i++) {
             if (Arrays.binarySearch(bukva,(((b[i].toLowerCase()).toCharArray())[0]))>0)
             {
-                if (b[i].length()==(a+1) || b[i].length()==a)
-                    for (int j = 0; j <bukva.length ; j++) {
-
-                if((b[i].toCharArray())[b[i].length()-1]==','|| ((b[i].toCharArray())[b[i].length()-1]=='.'))
-                {
-                    if (b[i].length()==(a+1))
+                if (b[i].length()==(a+1)&&((b[i].toCharArray())[b[i].length()-1]==','|| ((b[i].toCharArray())[b[i].length()-1]=='.')))
                         l.remove(b[i]);
-                }
-                    else
-                        if (b[i].length()==a)
-                            l.remove(b[i]);}
+                if ((b[i].length()==a) &&((b[i].toCharArray())[b[i].length()-1]!=',' && ((b[i].toCharArray())[b[i].length()-1]!='.')))
+                        l.remove(b[i]);
             }
         }
         for (int i = 0; i <(l.toArray()).length ; i++) {
@@ -35,7 +27,5 @@ public class Labor3 {
 
         }
         System.out.println(z.toString());
-        }
-
+    }
 }
-
